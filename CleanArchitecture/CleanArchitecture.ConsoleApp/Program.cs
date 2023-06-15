b88a331c4d91ab2e3ab6c52c530d624ba59e376c
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-15 17:46:42 -0500

    First Main

---
+++ CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,124 @@
+﻿
+using CleanArchitecture.Data;
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Security.Cryptography;
+
+StreamerDbContext dbContext = new();
+
+
+await TrackingAndNotTracking();
+///await QueryLinq();
+///await QueryMethods();
+//await AddNewRecords();
+///await QueryFilter();
+//QueryStreaming();
+Console.WriteLine("Presione cualquier tecla para terminar el programa");
+Console.ReadKey();
+
+async Task TrackingAndNotTracking()
+{
+    var StreamerWithtracking = await dbContext!.Streamers!.FirstOrDefaultAsync(x=>x.Id==1);
+    var StreamerWithNotracking = await dbContext!.Streamers!.AsNoTracking().FirstOrDefaultAsync(x => x.Id == 2);
+
+    StreamerWithtracking.Nombre = "Disney+";
+    StreamerWithNotracking.Nombre = "Disney+";
+
+    await dbContext!.SaveChangesAsync();
+}
+
+async Task QueryLinq()
+{
+    Console.WriteLine($"Ingrese el servicio de streaming");
+    var streamerNombre = Console.ReadLine(); 
+    var streamers = await (from i in dbContext.Streamers where EF.Functions.Like(i.Nombre,$"%{streamerNombre}") select i).ToListAsync();
+
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id}-{streamer.Nombre}");
+        
+    }
+}
+
+async Task QueryMethods()
+{
+    var streamer = dbContext!.Streamers!;
+    var firstAsync = await dbContext!.Streamers!.Where(y=>y.Nombre.Contains("a")).FirstAsync();
+    var firstOrDefaultAsync = await dbContext!.Streamers!.Where(y => y.Nombre.Contains("a")).FirstOrDefaultAsync();
+    var firstOrDefault_v2 = await dbContext!.Streamers!.FirstOrDefaultAsync(y => y.Nombre.Contains("a"));
+
+    var singleAsync = await streamer.Where(y => y.Id==1).SingleAsync();
+    var singleOrDefaultAsync = await streamer.Where(y => y.Id == 1).SingleOrDefaultAsync();
+    var resultado = await streamer.FindAsync(1);
+
+}
+async Task QueryFilter()
+{
+    Console.WriteLine($"Ingrese una compañia de Streaming: ");
+    var streamingNombre = Console.ReadLine();
+    var streamers = await dbContext!.Streamers!.Where(x=>x.Nombre.Equals(streamingNombre)).ToListAsync();
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id}-{streamer.Nombre}");
+    }
+
+    ///var streamerPartialResults = await dbContext!.Streamers!.Where(x=>x.Nombre.Contains(streamingNombre)).ToListAsync();
+    var streamerPartialResults = await dbContext!.Streamers!.Where(x => EF.Functions.Like(x.Nombre,$"%{streamingNombre}%")).ToListAsync();
+
+    foreach (var streamer in streamerPartialResults)
+    {
+        Console.WriteLine($"{streamer.Id}-{streamer.Nombre}");
+    }
+}
+void QueryStreaming()
+{
+    var streamers = dbContext!.Streamers!.ToList();
+
+    foreach (var streamer in streamers)
+    {
+        Console.WriteLine($"{streamer.Id}-{streamer.Nombre}");
+    }
+}
+
+async Task AddNewRecords()
+{
+    Streamer streamer = new()
+    {
+        Nombre = "Disney+",
+        Url = "https://www.disneyplus.com"
+    };
+
+    dbContext!.Streamers!.Add(streamer);
+
+    await dbContext.SaveChangesAsync();
+
+    var movies = new List<Video>
+{
+    new Video
+    {
+        Nombre="Dumbo",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="El Rey Leon",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="Aladin",
+        StreamerId=streamer.Id
+    },
+    new Video
+    {
+        Nombre="Frozen",
+        StreamerId=streamer.Id
+    }
+};
+    await dbContext.AddRangeAsync(movies);
+    await dbContext.SaveChangesAsync();
+
+}
---
+++ CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=SQL2019\DLLO,14333;Initial Catalog=PRACTICA;User ID=practica;Password=practica;TrustServerCertificate=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        
+        public DbSet<Video>? Videos { get; set; }
+
+    }
+}
