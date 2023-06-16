f1eca04f8283a377b1c51849397899afc04ab8cf
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-16 15:34:54 -0500

    Seccion 7 clase 37 Query Command y Handlers

---
+++ CleanArchitecture/CleanArchitecture.Application/Features/Videos/Queries/GetVideoList/GetVideosListQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Videos.Queries.GetVideoList
+{
+    internal class GetVideosListQuery
+    {
+    }
+}
