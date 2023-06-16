b388d8079615e3a3e427fa12a028c3fe39c662d4
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-16 11:45:25 -0500

    Seccion 5 Clase 31 Creacionn de interface generica

--- CleanArchitecture/CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture/CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -10,7 +10,7 @@ namespace CleanArchitecture.Domain.Common
     public abstract class BaseDomainModel
     {
         public int Id { get; set; }
-        public DateTime? DateCreated { get; set; }
+        public DateTime? CreateDate { get; set; }
         public String CreateBy { get; set; }
         public DateTime? LastModifiedDay { get; set; }
         public String LastModifiedBy { get; set; }
