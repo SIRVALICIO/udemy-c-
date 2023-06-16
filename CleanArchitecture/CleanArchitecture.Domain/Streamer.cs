d300a286f42db8df2e5a527dfea278030fa3b13e
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-16 10:30:33 -0500

    Seccion 3 Clase 24 Base model

--- CleanArchitecture/CleanArchitecture.Domain/Streamer.cs
+++ CleanArchitecture/CleanArchitecture.Domain/Streamer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArchitecture.Domain.Common;
 
 namespace CleanArchitecture.Domain
 {
