4f3fa005b33af7fac8e9122205154b59c0f5cf16
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-16 10:38:30 -0500

    Seccion 3 clase 25 Creacion de API

--- CleanArchitecture/CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture/CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.Common
 {
-    public class BaseDomainModel
+    public abstract class BaseDomainModel
     {
         public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public String CreateBy { get; set; }
+        public DateTime? LastModifiedDay { get; set; }
+        public String LastModifiedBy { get; set; }
+
     }
 }
