9c6ce9928a90d8aeb111cf342ce298b2b3450254
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-17 18:23:03 -0500

    Actualizacion

--- CleanArchitecture/CleanArchitecture.Data/Persistence/StreamerDbContextSeed.cs
+++ CleanArchitecture/CleanArchitecture.Data/Persistence/StreamerDbContextSeed.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Infrastructure.Persistence
 {
-    internal class StreamerDbContextSeed
+    public class StreamerDbContextSeed
     {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed> logger)
+        {
+            if (!context.Streamers.Any())
+            {
+                context.Streamers.AddRange(GetPreconfiguredStreamer());
+                context.SaveChangesAsync();
+                logger.LogInformation("Estamos insertando nuevos records al db {context}",typeof(StreamerDbContext).Name);
+            }
+
+
+
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamer()
+        {
+            return new List<Streamer>
+            {
+
+                new Streamer
+                {
+                    CreateBy="Alguien",
+                    Nombre="Hbo Al revez",
+                    Url="https://HBOAlRevez.com"
+
+                },
+
+                new Streamer
+                {
+                    CreateBy="Lucho",
+                    Nombre="Amazonuc",
+                    Url="https://Amazonuc.com"
+
+                }
+
+
+
+            };
+
+
+        }
+
+
     }
 }
