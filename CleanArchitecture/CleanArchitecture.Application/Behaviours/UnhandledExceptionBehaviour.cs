95984dc57dbc30587dec0e58b3a9c83e1d49b61f
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-17 17:28:39 -0500

    Seccion 10 clase 45 Crear depdendencias en proyecto Application

--- CleanArchitecture/CleanArchitecture.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ CleanArchitecture/CleanArchitecture.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Application.Behaviours
 {
-    internal class UnhandledExceptionBehaviour
+    internal class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
+
+        private readonly ILogger<TRequest  > _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+
+
+            }catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application REquest: Sucedio una exception para el request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
     }
 }
