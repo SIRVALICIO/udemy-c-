48af3330c110ebcf9294c705577da475ee0c44bc
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-18 11:47:53 -0500

    seccion 12 capitulo 49 Metodos Genericos

--- CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
+++ CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -22,7 +22,7 @@ namespace CleanArchitecture.Application.Contracts.Persistence
 
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task DeleteAsync(T entity);
 
         Task<T> UpdateAsync(T entity);
 
