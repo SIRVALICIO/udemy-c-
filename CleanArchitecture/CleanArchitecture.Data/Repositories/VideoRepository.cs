09960b3f652fc4a48130832e2ebf853a1aa0c435
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-18 12:02:46 -0500

    Seccion 12 capitul 50 Implementar metodos personalizados

---
+++ CleanArchitecture/CleanArchitecture.Data/Repositories/VideoRepository.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Repositories
+{
+    public class VideoRepository : RepositoryBase<Video>, IVideoRepository
+
+
+
+
+
+    {
+        public VideoRepository(StreamerDbContext context) : base(context)
+        {
+
+        }
+
+
+        public async Task<Video> GetVideoByNombre(string nombreVideo)
+        {
+            return  await _context.Videos.Where(v => v.Nombre == nombreVideo).FirstOrDefaultAsync();
+
+        }
+
+        async Task<IEnumerable<Video>> GetVideoByUsername(string username)
+        {
+            return await _context.Videos!.Where(v=>v.CreateBy==username).ToListAsync();
+        }
+    }
+}
