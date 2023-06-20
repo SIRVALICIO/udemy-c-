bfba188255b8201bb01630bd619bf4fa8452a3d4
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-20 14:14:35 -0500

    Seccion 16 capitulo 67 Implementacion de Unit of Work

--- CleanArchitecture/CleanArchitecture.Data/Repositories/RepositoryBase.cs
+++ CleanArchitecture/CleanArchitecture.Data/Repositories/RepositoryBase.cs
@@ -21,7 +21,7 @@ namespace CleanArchitecture.Infrastructure.Repositories
         }
 
         public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
-        { 
+        {
                 return await _context.Set<T>().Where(predicate).ToListAsync();
         }
 
@@ -39,7 +39,7 @@ namespace CleanArchitecture.Infrastructure.Repositories
 
             if(orderBy!=null)
                 return await orderBy(query).ToListAsync();
-        
+
 
             return await query.ToListAsync();
         }
@@ -48,8 +48,8 @@ namespace CleanArchitecture.Infrastructure.Repositories
                                      Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                      List<Expression<Func<T, object>>> includes = null,
                                      bool disableTracking = true)
-        { 
-        
+        {
+
             IQueryable<T> query = _context.Set<T>();
             if (disableTracking) query = query.AsNoTracking();
 
@@ -70,14 +70,15 @@ namespace CleanArchitecture.Infrastructure.Repositories
         }
 
         public async Task<T> AddAsync(T entity)
-        { 
+        {
             _context.Set<T>().Add(entity);
             await _context.SaveChangesAsync();
-            return entity;  
+            return entity;
         }
 
         public async Task<T> UpdateAsync(T entity)
-        { 
+        {
+            _context.Set<T>().Attach(entity);
             _context.Entry(entity).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return entity;
@@ -89,5 +90,26 @@ namespace CleanArchitecture.Infrastructure.Repositories
             await _context.SaveChangesAsync();
         }
 
+        public void AddEntity(T entity)
+        {
+            _context.Set<T>().Add(entity);
+
+
+
+
+        }
+
+        public void UpdateEntity(T entity)
+        {
+
+            _context.Set<T>().Attach(entity);
+
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void DeleteEntity(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
     }
 }
