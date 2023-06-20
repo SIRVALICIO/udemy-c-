14dc7c35958d33d60bb1e45cf0c97700f9f3a1dd
Camilo Serrano <47458199+valquero779@users.noreply.github.com>
2023-06-20 15:22:16 -0500

    Seccion 16 capitulo 70  Unit Of Work para Custom repositories

--- CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
+++ CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchitecture.Domain.Common;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,17 @@ using System.Threading.Tasks;
 namespace CleanArchitecture.Application.Contracts.Persistence
 {
     public interface  IUnitOfWork:IDisposable
+
+
+
     {
+
+
+
+
+        IStreamerRepository StreamerRepository { get; }
+        IVideoRepository VideoRepository { get; }
+
         IAsyncRepository<TEntity> Repository<TEntity>() where TEntity: BaseDomainModel;
 
         Task<int> Complete();
--- CleanArchitecture/CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandHandler.cs
+++ CleanArchitecture/CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandHandler.cs
@@ -15,14 +15,16 @@ namespace CleanArchitecture.Application.Features.Streamers.Commands
 {
     public class CreateStreamerCommandHandler : IRequestHandler<CreateStreamerCommand, int>
     {
-        private readonly IStreamerRepository _streamerRepository;
+        //private readonly IStreamerRepository _streamerRepository;
+
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IEmailService _emailservice;
         private readonly ILogger<CreateStreamerCommandHandler> _logger;
 
-        public CreateStreamerCommandHandler(IStreamerRepository streamerRepository, IMapper mapper, IEmailService emailservice, ILogger<CreateStreamerCommandHandler> logger)
+        public CreateStreamerCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, IEmailService emailservice, ILogger<CreateStreamerCommandHandler> logger)
         {
-            _streamerRepository = streamerRepository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
             _emailservice = emailservice;
             _logger = logger;
@@ -31,13 +33,21 @@ namespace CleanArchitecture.Application.Features.Streamers.Commands
         public async Task<int> Handle(CreateStreamerCommand request, CancellationToken cancellationToken)
         {
             var streamerEntity = _mapper.Map<Streamer>(request);
-            var newStreamer = await _streamerRepository.AddAsync(streamerEntity);
+            //var newStreamer = await _streamerRepository.AddAsync(streamerEntity);
+             _unitOfWork.StreamerRepository.AddEntity(streamerEntity);
+            var result = await _unitOfWork.Complete();
 
-            _logger.LogInformation($"Streamer {newStreamer.Id} fue creado existosamente");
+            if (result <= 0)
+            {
+                throw new Exception($"No se pudo insetar el record de stramer");
+            }
 
-            await SendEmail(newStreamer);
 
-            return newStreamer.Id;
+            _logger.LogInformation($"Streamer {streamerEntity.Id} fue creado existosamente");
+
+            await SendEmail(streamerEntity);
+
+            return streamerEntity.Id;
         }
 
         private async Task SendEmail(Streamer streamer)
